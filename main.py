f5bc7f9e7326be0322a0005764d294230c12c000
용굴이 <72863449+wldnror@users.noreply.github.com>
2026-06-10 15:22:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,84 @@
-import spidev
 import time
+from collections import deque
+
+import spidev
+import matplotlib.pyplot as plt
+
 
 spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz = 50000
 spi.mode = 0
 
-def read_reg(reg):
+
+REG_CHIP_ID = 0xD0
+REG_RESET = 0xE0
+REG_STATUS = 0x73
+
+REG_CTRL_GAS_1 = 0x71
+REG_CTRL_HUM = 0x72
+REG_CTRL_MEAS = 0x74
+REG_CONFIG = 0x75
+
+REG_RES_HEAT_0 = 0x5A
+REG_GAS_WAIT_0 = 0x64
+REG_FIELD0 = 0x1D
+
+
+def read_reg_raw(reg):
     return spi.xfer2([reg | 0x80, 0x00])[1]
 
-def write_reg(reg, val):
-    spi.xfer2([reg & 0x7F, val & 0xFF])
 
-write_reg(0xE0, 0xB6)
-time.sleep(0.2)
+def write_reg_raw(reg, value):
+    spi.xfer2([reg & 0x7F, value & 0xFF])
 
-write_reg(0x75, 0x08)
-write_reg(0x72, 0x01)
-write_reg(0x5A, 0x73)
-write_reg(0x64, 0x59)
-write_reg(0x71, 0x20)
-write_reg(0x74, (0b010 << 5) | (0b101 << 2) | 0b01)
 
-time.sleep(1.0)
+def set_mem_page(reg):
+    status = read_reg_raw(REG_STATUS)
 
-for r in [0xD0, 0x75, 0x72, 0x5A, 0x64, 0x71, 0x74]:
-    print(hex(r), "=", hex(read_reg(r)))
+    if reg < 0x80:
+        status &= ~0x10
+    else:
+        status |= 0x10
 
-data = spi.xfer2([0x1D | 0x80] + [0x00] * 17)[1:]
-print("FIELD0:", [hex(x) for x in data])
+    write_reg_raw(REG_STATUS, status)
+    time.sleep(0.001)
+
+
+def read_reg(reg):
+    set_mem_page(reg)
+    return read_reg_raw(reg)
+
+
+def write_reg(reg, value):
+    set_mem_page(reg)
+    write_reg_raw(reg, value)
+
+
+def read_regs(reg, length):
+    set_mem_page(reg)
+    return spi.xfer2([reg | 0x80] + [0x00] * length)[1:]
+
+
+def sensor_reset():
+    write_reg(REG_RESET, 0xB6)
+    time.sleep(0.2)
+
+
+def sensor_init():
+    chip_id = read_reg(REG_CHIP_ID)
+    print("Chip ID:", hex(chip_id))
+
+    if chip_id != 0x61:
+        raise RuntimeError("BME688/BME680 chip ID error")
+
+    sensor_reset()
+
+    print("Chip ID after reset:", hex(read_reg(REG_CHIP_ID)))
+
+    write_reg(REG_CONFIG, 0x08)
+    write_reg(REG_CTRL_HUM, 0x01)
+
+    # Gas heater test setting
+    write_reg(REG_RES_HEAT_0, 0x73)
+    write_reg(REG
